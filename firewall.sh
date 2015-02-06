c3fe52f26e2cce6f8dbd24b427b38ac7066ac09d
cstyan <callumstyan@gmail.com>
2015-02-06 15:53:30 -0800

    protocols twice

--- firewall.sh
+++ firewall.sh
@@ -17,7 +17,7 @@ ICMPSERVICES=()
 for i in ${TCPPORTS[@]}
 do
 	#inbound forwarded tcp packets on DNS, HTTP, HTTPS
-	iptables -A FORWARD -i em1 -o p3p1 -p tcp --sport $i -p tcp -j ACCEPT  
+	iptables -A FORWARD -i em1 -o p3p1 -p tcp --sport $i -j ACCEPT  
 	#outbound forwarded tcp packets
-	iptables -A FORWARD -o em1 -i p3p1 -p tcp --dport $i -p tcp -j ACCEPT  
+	iptables -A FORWARD -o em1 -i p3p1 -p tcp --dport $i -j ACCEPT  
 done

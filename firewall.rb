16d8bacc7e3926a0c3d93a21519d8c507f28f8b1
cstyan <callumstyan@gmail.com>
2015-02-06 20:22:28 -0800

    more stuff and things, they're nice

--- firewall.rb
+++ firewall.rb
@@ -42,17 +42,74 @@ def userParams
 	@icmpServices = tcpServices.split(",")
 end
 
+def drop
+	#all inbound packets to ports less than 1024
+	`iptables -A FORWARD -i em1 -o p3p1 -p tcp,udp --sport 0:1023 -j drop`
+	#drop incoming christmas tree packets
+	`iptables -A FORWARD -p tcp -i em1 --tcp-flags ALL ALL -j drop`
+	#drop outbound christmas tree packets
+	`iptables -A FORWARD -p tcp -o em1 --tcp-flags ALL ALL -j drop`
+	#drop incoming null scan packets
+	`iptables -A FORWARD -p tcp -i em1 --tcp-flags ALL NONE -j drop`
+	#drop outbound null scan packets
+	`iptables -A FORWARD -p tcp -0 em1 --tcp-flags ALL NONE -j drop`
+
+	#drop anything that gets forwarded to drop chain
+	`iptables -A drop -j DROP`
+end
+
+def removeUserChains
+	#tcpIn
+	`iptables -X tcpIn`
+	#tcpOut
+	`iptables -X tcpOut`
+	#udpIn
+	`iptables -X udpIn`
+	#udpOut
+	`iptables -X udpOut`
+	#icmpIn
+	`iptables -X icmpIn`
+	#icmpOut
+	`iptables -X icmpOut`
+	#general drop rules
+	`iptables -X drop`
+	#others?
+	`iptables -X other`
+end
+
+def createUserChains
+	#tcpIn
+	`iptables -N tcpIn`
+	#tcpOut
+	`iptables -N tcpOut`
+	#udpIn
+	`iptables -N udpIn`
+	#udpOut
+	`iptables -N udpOut`
+	#icmpIn
+	`iptables -N icmpIn`
+	#icmpOut
+	`iptables -N icmpOut`
+	#general drop rules
+	`iptables -N drop`
+	#others?
+	`iptables -N other`
+end
+
 def writeTCP
 	run = 0
 	while run < @tcpServices.length
 		#convert the current type to a string for the iptables command
 		port = @tcpServices[run]
 		#inbound forwarded tcp packets on DNS, HTTP, HTTPS
-		`iptables -A FORWARD -i #{@internalInterface}  -o #{@externalInterface} -p tcp --dport #{port} -m state--state NEW,ESTABLISHED`
+		`iptables -A FORWARD -i #{@internalInterface}  -o #{@externalInterface} -p tcp --dport #{port} -m state--state NEW,ESTABLISHED -j tcpIn`
 		#outbound forwarded tcp packets on DNS, HTTP, HTTPS
-		`iptables -A FORWARD -o #{@internalInterface}  -i #{@externalInterface} -p tcp --dport #{port} -m state --state NEW,ESTABLISHED`
+		`iptables -A FORWARD -o #{@internalInterface}  -i #{@externalInterface} -p tcp --dport #{port} -m state --state NEW,ESTABLISHED -j tcpOut`
 		run = run + 1
 	end
+	#accept everything that gets forwarded to tcpIn and tcpOut
+	`iptables -A tcpIn -j ACCEPT`
+	`iptables -A tcpOut -j ACCEPT`
 end
 
 def writeUDP
@@ -61,11 +118,14 @@ def writeUDP
 		#convert the current type to a string for the iptables command
 		port = @udpServices[run]
 		#inbound forwarded UDP packets
-		`iptables -A FORWARD -i #{@internalInterface}  -o #{@externalInterface} -p udp --dport #{port} -m state --state NEW,ESTABLISHED`
+		`iptables -A FORWARD -i #{@internalInterface}  -o #{@externalInterface} -p udp --dport #{port} -m state --state NEW,ESTABLISHED -j udpIn`
 		#outbound forwarded UDP packets
-		`iptables -A FORWARD -o #{@internalInterface}  -i #{@externalInterface} -p udp --dport #{port} -m state --state NEW,ESTABLISHED`
+		`iptables -A FORWARD -o #{@internalInterface}  -i #{@externalInterface} -p udp --dport #{port} -m state --state NEW,ESTABLISHED -j udpOut`
 		run = run + 1
 	end
+	#accept everything that gets forwarded to udpIn and udpOut
+	`iptables -A udpIn -j ACCEPT`
+	`iptables -A udpOut -j ACCEPT`
 end
 
 def writeICMP
@@ -74,30 +134,41 @@ def writeICMP
 		#convert the current type to a string for the iptables command
 		typeString = @icmpServices[run]
 		#inbound ICMP on allowed ports
-		`iptables -A FORWARD -i #{@internalInterface} -o #{@externalInterface} -p icmp --icmp-type #{typeString} -m state --state NEW,ESTABLISHED -j ACCEPT`
+		`iptables -A FORWARD -i #{@internalInterface} -o #{@externalInterface} -p icmp --icmp-type #{typeString} -m state --state NEW,ESTABLISHED -j icmpIn`
 		#outbound ICMP on allowed ports
-		`iptables -A FORWARD -i #{@externalInterface} -o #{@internalInterface} -p icmp --icmp-type #{typeString} -m state --state NEW,ESTABLISHED -j ACCEPT`
+		`iptables -A FORWARD -i #{@externalInterface} -o #{@internalInterface} -p icmp --icmp-type #{typeString} -m state --state NEW,ESTABLISHED -j icmpOut`
 		run = run + 1
 	end
+	#accept everything that gets forwarded to to icmpIn and icmpOut
+	`iptables -A icmpIn -j ACCEPT`
+	`iptables -A icmpIn -j ACCEPT`
 end
 
+
+def configureNAT
+	puts "nat stuff"
+end
 #start of the firewall
 def writeFirewall
 	#flush existing tables
-	puts "flushing firewall"
-	`sudo iptables -F`
-	#clear any user defined chains we have
-
-	#write tcp
-	#writeTCP
+	puts "flushing IPTables"
+	`iptables -F`
+	#clear any previous user defined chains we have created
+	removeUserChains
+	#create user chains
+	createUserChains
+	puts "writing drop rules to IPTables"
+	drop
+	puts "writing accept rules to IPTables"	
+	writeTCP
 	writeUDP
 	writeICMP
-	#write udp
-	#write icmp
 end
 
 def main
-	#userParams
+	#we should call config nat here
+
+	createUserChains	
 	writeFirewall
 end
 

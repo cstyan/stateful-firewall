7fa125459fae3e6b1f95db254f226c15466ba2b3
cstyan <callumstyan@gmail.com>
2015-02-12 01:06:40 -0800

    external tests seem to work

--- externalTest.rb
+++ externalTest.rb
@@ -1,34 +1,5 @@
 @ip
 
-def test1024
-	puts "Test 1: Drop all incomming packets to low ports (less than 1024)."
-	puts "-- TCP --"
-	`hping3 #{@ip} -c 1 -V -p 22 -s 22 -S`
-	`hping3 #{@ip} -c 1 -V -p 22 -s 8080 -S`
-	puts "-- UDP --"
-	`hping3 #{@ip} -c 1 -V -p 22 -s 22 --udp`
-	`hping3 #{@ip} -c 1 -V -p 22 -s 8080 ---udp`
-end
-
-def testChristmas
-	puts "Test2: Block incomming Christmans Tree scans."
-	`hping3 #{@ip} -c 5 -V -p 8000 -s 8080 -SFRUPA`
-end
-
-def testNull
-	puts "Test3: Block incmming Null scans."
-	`hping3 #{@ip} -c 5 -V -p 8000 -s 8080`
-end
-
-def testSynFin
-	puts "Test 4: Block incomming packets"
-	`hping3 #{@ip}-c 5 -V -p 8080 -s 8080 -SF`
-end
-
-def testTcp
-	puts "shit"
-end
-
 def testSF
 	`hping3 #{@ip} -c 1 -s 22 -p 8888 -SF`
 end
@@ -56,10 +27,10 @@ end
 
 #ports required to be dropped via assignment requirements
 def dropStatic
-	`hping 3 #{@ip} -c 1 -p 32768 -s 80 -S`
-	`hping 3 #{@ip} -c 1 -p 138 -s 80 -S`
-	`hping 3 #{@ip} -c 1 -p 32768 -s 80 --udp`
-	`hping 3 #{@ip} -c 1 -p 138 -s 80 --udp`
+	`hping3 #{@ip} -c 1 -p 32768 -s 80 -S`
+	`hping3 #{@ip} -c 1 -p 138 -s 80 -S`
+	`hping3 #{@ip} -c 1 -p 32768 -s 80 --udp`
+	`hping3 #{@ip} -c 1 -p 138 -s 80 --udp`
 	`hping3 #{@ip} -c 1 -p 111 -s 80 -S`
 	`hping3 #{@ip} -c 1 -p 515 -s 80 -S`
 end
